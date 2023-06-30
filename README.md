0f02f9ce478c7e4d2d310360ec21b988e57d088d
Vatsalsoni13 <sonivatsal2001@gmail.com>
2023-06-30 18:30:06 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Blockchain assignment 4
---
+++ hardhat.config.js
@@ -0,0 +1,20 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
+module.exports = {
+   solidity: "0.8.11",
+   networks: {
+      volta: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`],
+         gas: 210000000,
+         gasPrice: 800000000000,
+      }
+   },
+}
